90d08554c872f28a9c59f18d96d82d79b8b1a82f
duyquang20091995@gmail.com <duyquang20091995@gmail.com>
2019-05-14 00:23:09 +0900

    The last applied

---
+++ GitPractise/src/Animal.java
@@ -0,0 +1,35 @@
+
+public class Animal {
+	public int id;
+	public String name;
+
+	public Animal(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Animal [id=" + id + ", name=" + name + "]";
+	}
+
+
+
+}
--- GitPractise/src/TestResult.java
+++ GitPractise/src/TestResult.java
@@ -4,10 +4,18 @@ import java.util.List;
 public class TestResult {
 	public static void main(String[] args) {
 		List<Person> listpeople = new ArrayList<Person>();
+		List<Animal> listanimals = new ArrayList<Animal>();
 		listpeople.add(new Person(1, "Qu�ch Duy Quang", 24, "298 Kim Ng�u", "abc.jpg", "25000"));
 
 		for (Person person : listpeople) {
 			System.out.println(person);
+			System.out.println();
+		}
+
+		listanimals.add(new Animal(1, "con bò"));
+
+		for (Animal animal : listanimals) {
+			System.out.println(animal);
 			System.out.println();
 		}
 	}
